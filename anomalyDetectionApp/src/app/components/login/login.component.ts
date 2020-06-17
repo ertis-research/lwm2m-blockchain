427eb34679597ad96ec73550c5502a0b8d306ce4
Ivan <ivaann2706@gmail.com>
2020-06-17 23:53:49 +0200

    ANOMALYAPP - Enhanced login

--- anomalyDetectionApp/src/app/components/login/login.component.ts
+++ anomalyDetectionApp/src/app/components/login/login.component.ts
@@ -7,14 +7,19 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: ['.invalid { color: red; font-weight: bold; }']
+  styles: [
+    '.invalid { color: red; font-weight: bold; }',
+    '.radio-div { display: flex; justify-content: center; }',
+  ]
 })
 export class LoginComponent implements OnInit{
 
   urlServer: string;
-  wildcard: string;
+  username: string;
+  email: string;
   password: string;
   message: string;
+  option: number;
 
   constructor(
     private router: Router,
@@ -29,10 +34,17 @@ export class LoginComponent implements OnInit{
         alert('Your token has expired. You must log in again.');
       }
     });
+    this.option = 1;
   }
 
   validateLogin() {
-    this.login.validateLogin(this.wildcard,this.password, this.urlServer)
+    let wildcard = "";
+    if(this.option === 1) {
+      wildcard = this.username;
+    }else{
+      wildcard = this.email;
+    }
+    this.login.validateLogin(wildcard,this.password, this.urlServer)
       .subscribe((response: Credentials) => {
         this.message = undefined;
         this.auth.setUserCredentials(response);
