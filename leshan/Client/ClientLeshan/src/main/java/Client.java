d9f354f60052fb27fe5e0c5fc8f84e2188a0d9c1
Ivan <ivaann2706@gmail.com>
2020-06-22 23:44:00 +0200

    LESHAN - Using LwM2M object model of temperature sensor (Client)

--- leshan/Client/ClientLeshan/src/main/java/Client.java
+++ leshan/Client/ClientLeshan/src/main/java/Client.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import org.eclipse.leshan.LwM2mId;
 import org.eclipse.leshan.client.californium.LeshanClient;
 import org.eclipse.leshan.client.californium.LeshanClientBuilder;
@@ -5,6 +7,9 @@ import org.eclipse.leshan.client.object.Device;
 import org.eclipse.leshan.client.object.Security;
 import org.eclipse.leshan.client.object.Server;
 import org.eclipse.leshan.client.resource.ObjectsInitializer;
+import org.eclipse.leshan.core.model.ObjectLoader;
+import org.eclipse.leshan.core.model.ObjectModel;
+import org.eclipse.leshan.core.model.StaticModel;
 import org.eclipse.leshan.core.request.BindingMode;
 
 public class Client {
@@ -14,15 +19,21 @@ public class Client {
 		byte[] idByte = asciiToByte("ivanbs1");
 		byte[] keyByte = hexToByte("654321");
 		String url = "coaps://localhost:5684";		
+		int OBJECT_ID_TEMPERATURE_SENSOR = 3303;
+			
+		//Load model
+		List<ObjectModel> models = ObjectLoader.loadDefault();
+		String[] modelPaths = new String[] {"3303.xml"};
+		models.addAll(ObjectLoader.loadDdfResources("/models", modelPaths));
 		
 		LeshanClientBuilder builder = new LeshanClientBuilder(endpoint);
 		
 		// create objects
-		ObjectsInitializer initializer = new ObjectsInitializer();
+		ObjectsInitializer initializer = new ObjectsInitializer(new StaticModel(models));
 		initializer.setInstancesForObject(LwM2mId.SECURITY, Security.pskBootstrap(url, idByte, keyByte));
 		initializer.setInstancesForObject(LwM2mId.SERVER, new Server(12345, 5 * 60, BindingMode.U, false));
 		initializer.setInstancesForObject(LwM2mId.DEVICE, new Device("Eclipse Leshan", "model12345", "12345", "U"));
-		initializer.setInstancesForObject(7, new TemperatureSensor());
+		initializer.setInstancesForObject(OBJECT_ID_TEMPERATURE_SENSOR, new TemperatureSensor());
 
 		// add it to the client
 		builder.setObjects(initializer.createAll());
@@ -31,7 +42,6 @@ public class Client {
 		// start client
 		client.start();
 	}
-
 	
 	private static byte[] hexToByte(String s) {
 		int len = s.length();
--- leshan/Client/ClientLeshan/src/main/java/TemperatureSensor.java
+++ leshan/Client/ClientLeshan/src/main/java/TemperatureSensor.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 
 import org.eclipse.leshan.client.request.ServerIdentity;
@@ -5,18 +7,26 @@ import org.eclipse.leshan.client.resource.BaseInstanceEnabler;
 import org.eclipse.leshan.core.response.ReadResponse;
 
 public class TemperatureSensor extends BaseInstanceEnabler{
+		
 	@Override
 	public ReadResponse read(ServerIdentity identity, int resourceid) {
 		System.out.println("ReadResponse "+resourceid + " " +identity.getPskIdentity());
 		switch (resourceid) {
-		case 0:
-			return ReadResponse.success(resourceid, getRandom());
+		case 5700:
+			return ReadResponse.success(resourceid, getTwoDigitValue(getRandom()));
 		}
 		return ReadResponse.notFound();
 	}
 	
-	private int getRandom() {
-		Random rand = new Random();
-		return rand.nextInt(60);
+	private double getRandom() {
+		double leftLimit = 1;
+	    double rightLimit = 60;
+	    double generatedRandom = leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
+		return generatedRandom;
 	}
+	
+	private double getTwoDigitValue(double value) {
+        BigDecimal toBeTruncated = BigDecimal.valueOf(value);
+        return toBeTruncated.setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
 }
