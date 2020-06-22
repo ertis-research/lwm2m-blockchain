a458098182b7f3f29ae276775ce3380b00123ef7
Ivan <ivaann2706@gmail.com>
2020-06-23 00:03:44 +0200

    LESHAN - Using LwM2M object model of temperature sensor (Server)

--- leshan/Server/ServerLeshan/src/main/java/com/ivan/ServerLeshan/service/LeshanServerService.java
+++ leshan/Server/ServerLeshan/src/main/java/com/ivan/ServerLeshan/service/LeshanServerService.java
@@ -8,30 +8,49 @@ import java.util.List;
 
 import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.scandium.config.DtlsConnectorConfig;
+import org.eclipse.leshan.core.model.ObjectLoader;
+import org.eclipse.leshan.core.model.ObjectModel;
+import org.eclipse.leshan.core.node.LwM2mNode;
 import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.node.codec.DefaultLwM2mNodeDecoder;
 import org.eclipse.leshan.core.node.codec.DefaultLwM2mNodeEncoder;
 import org.eclipse.leshan.core.node.codec.LwM2mNodeDecoder;
 import org.eclipse.leshan.core.request.ReadRequest;
+import org.eclipse.leshan.core.response.LwM2mResponse;
 import org.eclipse.leshan.core.response.ReadResponse;
 import org.eclipse.leshan.server.californium.LeshanServer;
 import org.eclipse.leshan.server.californium.LeshanServerBuilder;
+import org.eclipse.leshan.server.model.LwM2mModelProvider;
+import org.eclipse.leshan.server.model.VersionedModelProvider;
 import org.eclipse.leshan.server.registration.Registration;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.ivan.ServerLeshan.bean.Client;
+import com.ivan.ServerLeshan.json.LwM2mNodeDeserializer;
+import com.ivan.ServerLeshan.json.LwM2mNodeSerializer;
+import com.ivan.ServerLeshan.json.RegistrationSerializer;
+import com.ivan.ServerLeshan.json.ResponseSerializer;
 
 @Service
 public class LeshanServerService {
 
+	private final Gson gson;
 	boolean supportDeprecatedCiphers = false;
-
 	String secureLocalAddress= "0.0.0.0";
 	int secureLocalPort = 5784;
 
 	LeshanServer leshanServer;
 
 	public LeshanServerService() {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeHierarchyAdapter(Registration.class, new RegistrationSerializer(leshanServer.getPresenceService()));
+        gsonBuilder.registerTypeHierarchyAdapter(LwM2mResponse.class, new ResponseSerializer());
+        gsonBuilder.registerTypeHierarchyAdapter(LwM2mNode.class, new LwM2mNodeSerializer());
+        gsonBuilder.registerTypeHierarchyAdapter(LwM2mNode.class, new LwM2mNodeDeserializer());
+        gsonBuilder.setDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        this.gson = gsonBuilder.create();
 		leshanServer = configureLeshanServer().build();
 		leshanServer.start();
 	}
@@ -45,7 +64,14 @@ public class LeshanServerService {
 	}
 	
 	private LeshanServerBuilder configureLeshanServer() {
+		String[] modelPaths = new String[] { "3303.xml"};
+		List<ObjectModel> models = ObjectLoader.loadDefault();
+        models.addAll(ObjectLoader.loadDdfResources("/models/", modelPaths));
+        LwM2mModelProvider modelProvider = new VersionedModelProvider(models);
+        
 		LeshanServerBuilder builder = new LeshanServerBuilder();
+		builder.setObjectModelProvider(modelProvider);
+		
 		builder.disableUnsecuredEndpoint();
 		builder.setLocalSecureAddress(secureLocalAddress, secureLocalPort);
 		builder.setEncoder(new DefaultLwM2mNodeEncoder());
@@ -96,8 +122,8 @@ public class LeshanServerService {
 		Client c = new Client(endpoint, registration.getRegistrationDate().getTime(), registration.getLastUpdate().getTime());
 				
 		try {
-			ReadResponse response = leshanServer.send(registration, new ReadRequest(7,0,0));
-			if(response.isSuccess()) {
+			ReadResponse response = leshanServer.send(registration, new ReadRequest(3303,0,5700));
+			if(response.isSuccess()) {				
 				Object value = ((LwM2mResource)response.getContent()).getValue();
 				c.setValue(Double.valueOf(value.toString()));
 				c.setValueTimestamp(new Timestamp(System.currentTimeMillis()).getTime());
