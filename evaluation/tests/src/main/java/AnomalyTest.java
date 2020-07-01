0542f27abe39fa7a8a2e5adc7e5a38ab91ce8cc0
Ivan <ivaann2706@gmail.com>
2020-07-01 12:18:45 +0200

    EVALUATION - Creation of the project to test

---
+++ evaluation/tests/src/main/java/AnomalyTest.java
@@ -0,0 +1,9 @@
+
+public class AnomalyTest {
+
+	public static void main(String[] args) {
+		System.out.println("Anomaly - test");
+
+	}
+
+}
---
+++ evaluation/tests/src/main/java/UserTest.java
@@ -0,0 +1,7 @@
+
+public class UserTest {
+	public static void main(String[] args) {
+		System.out.println("User - test");
+
+	}
+}
