884ed93784ad2ca5d2dc2adf64053b28306a881c
Ivan <ivaann2706@gmail.com>
2020-07-07 18:00:16 +0200

    LESHAN - Using new client smart contract on bootstrap server

--- leshan/BootstrapServer/BootstrapServerLeshan/src/main/java/BlockchainBootstrapStore.java
+++ leshan/BootstrapServer/BootstrapServerLeshan/src/main/java/BlockchainBootstrapStore.java
@@ -21,6 +21,7 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tuples.generated.Tuple6;
+import org.web3j.tuples.generated.Tuple7;
 import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.tx.gas.StaticGasProvider;
 
@@ -30,7 +31,7 @@ public class BlockchainBootstrapStore implements EditableBootstrapConfigStore {
 
 	String url = "https://ropsten.infura.io/v3/a21979a509154e19b42267c28f697e32"; //Ropsten
 	String privateKey = "4C2A99F86C06C98448AB1986D33A248D699B5D7280EEBD76E4FD60B84C4B51C8"; //Private key of an account, Ropsten
-	String contractAddress = "0x0b65c81b465953fd25b29c0caffd2a448f0b948f"; //Ropsten
+	String contractAddress = "0x6c8568e24548115439174e86d98fb8d7d3de2c4b"; //Ropsten
 	
 	//String url = "HTTP://127.0.0.1:7545"; //Ganache
 	//String privateKey = "a701158005907d33a130caa07a2b7d811b409336ba14efca9a00b8593ec6feb9"; //Private key of an account, Ganache
@@ -40,7 +41,7 @@ public class BlockchainBootstrapStore implements EditableBootstrapConfigStore {
 	BigInteger gasLimit = new BigInteger("4712388");
 	ContractGasProvider gasProvider = new StaticGasProvider(gasPrice, gasLimit);
 	Web3j web3j;
-	BootstrapStore contract;
+	ClientStore contract;
 
 	public BlockchainBootstrapStore() {
 		try {
@@ -59,11 +60,11 @@ public class BlockchainBootstrapStore implements EditableBootstrapConfigStore {
 		Map<String, BootstrapConfig> res = new ConcurrentHashMap<>();
 
 		try {
-			String[] endpoints = getAllClients(contract);
-			System.out.println(endpoints);
-			for (String end : endpoints) {
-				BootstrapConfig config = getClient(contract, end);
-				res.put(end, config);
+			List<BootstrapConfigWeb3> bootstrapConfigWeb3= getAllClients();
+			
+			for (BootstrapConfigWeb3 configWeb3 : bootstrapConfigWeb3) {
+				BootstrapConfig config = bootstrapConfigWeb3ToBootstrapConfig(configWeb3);
+				res.put(configWeb3.endpoint, config);
 			}
 
 		} catch (Exception e) {
@@ -102,6 +103,7 @@ public class BlockchainBootstrapStore implements EditableBootstrapConfigStore {
 	}
 
 	public class BootstrapConfigWeb3{
+		String endpoint;
 		String url_bs;
 		String id_bs;
 		String key_bs;
@@ -109,16 +111,18 @@ public class BlockchainBootstrapStore implements EditableBootstrapConfigStore {
 		String id_s;
 		String key_s;
 
-		public BootstrapConfigWeb3(byte[] url_bs, byte[] id_bs, byte[] key_bs,byte[]  url_s, byte[] id_s, byte[] key_s) {
-			this.url_bs = byteToAscii(url_bs);
-			this.id_bs = byteToAscii(id_bs);
-			this.key_bs = byteToAscii(key_bs);
-			this.url_s = byteToAscii(url_s);
-			this.id_s = byteToAscii(id_s);
-			this.key_s = byteToAscii(key_s);
+		public BootstrapConfigWeb3(String endpoint, String url_bs, String id_bs, String key_bs, String url_s, String id_s, String key_s) {
+			this.endpoint = endpoint;
+			this.url_bs = url_bs;
+			this.id_bs = id_bs;
+			this.key_bs = key_bs;
+			this.url_s = url_s;
+			this.id_s = id_s;
+			this.key_s = key_s;
 		}
 
-		public BootstrapConfigWeb3(Tuple6<byte[], byte[], byte[], byte[], byte[], byte[]> clientConfig) {
+		public BootstrapConfigWeb3(String endpoint, Tuple6<byte[], byte[], byte[], byte[], byte[], byte[]> clientConfig) {
+			this.endpoint = endpoint;
 			this.url_bs = byteToAscii(clientConfig.component1());
 			this.id_bs = byteToAscii(clientConfig.component2());
 			this.key_bs = byteToAscii(clientConfig.component3());
@@ -150,8 +154,8 @@ public class BlockchainBootstrapStore implements EditableBootstrapConfigStore {
 	}
 
 	// Load smart contract
-	private BootstrapStore loadContract(Web3j web3j, Credentials credentials, String address, ContractGasProvider provider) throws Exception {
-		BootstrapStore contract = BootstrapStore.load(address, web3j, credentials, provider);
+	private ClientStore loadContract(Web3j web3j, Credentials credentials, String address, ContractGasProvider provider) throws Exception {
+		ClientStore contract = ClientStore.load(address, web3j, credentials, provider);
 		log.info("Smart contract loaded");
 		return contract;
 	}
@@ -168,7 +172,7 @@ public class BlockchainBootstrapStore implements EditableBootstrapConfigStore {
 		log.info("Gas Used: " + gasUsed);
 	}
 
-	private BootstrapConfig getClient(BootstrapStore contract, String endpoint) throws Exception {
+	private BootstrapConfig getClient(ClientStore contract, String endpoint) throws Exception {
 		long startTime = System.currentTimeMillis();
 		Tuple6<byte[],byte[],byte[],byte[],byte[],byte[]> response = contract.getClient(asciiToByte32(endpoint)).send();
 		log.info("CLIENT ENDPOINT " + endpoint + " DATA STORED");
@@ -179,28 +183,12 @@ public class BlockchainBootstrapStore implements EditableBootstrapConfigStore {
 		log.info("Identity: " + byteToAscii(response.component5()));
 		log.info("Key: " + byteToAscii(response.component6()));
 
-		BootstrapConfigWeb3 bootstrapConfigWeb3 = new BootstrapConfigWeb3(response);
+		BootstrapConfigWeb3 bootstrapConfigWeb3 = new BootstrapConfigWeb3(endpoint, response);
 		BootstrapConfig bootstrapConfig = bootstrapConfigWeb3ToBootstrapConfig(bootstrapConfigWeb3);
 		long endTime = System.currentTimeMillis();
 		long totalTime = ((endTime - startTime));
 		System.out.println("getClient: "+ totalTime + " ms");
 		return bootstrapConfig;
-	}
-
-	private String[] getAllClients(BootstrapStore contract) throws Exception {
-		long startTime = System.currentTimeMillis();
-		@SuppressWarnings("unchecked")
-		List<byte[]> list = contract.getAllClients().send();
-		List<String> list2 = new ArrayList<String>();
-		for(byte[] aux : list) {
-			list2.add(byteToAscii(aux));
-		}
-		String[] response = new String[list.size()];
-		list2.toArray(response);
-		long endTime = System.currentTimeMillis();
-		long totalTime = ((endTime - startTime));
-		System.out.println("getAllClients: "+ totalTime + " ms");
-		return response;
 	}
 
 	private BootstrapConfig bootstrapConfigWeb3ToBootstrapConfig(BootstrapConfigWeb3 bootstrapConfigWeb3) {
@@ -236,6 +224,44 @@ public class BlockchainBootstrapStore implements EditableBootstrapConfigStore {
 		return serverConfig;
 	}
 
+	public List<BootstrapConfigWeb3> getAllClients(){
+		List<BootstrapConfigWeb3> configs = new ArrayList<BootstrapConfigWeb3>();
+
+		try {
+			Tuple7<List<byte[]>, List<byte[]>, List<byte[]>, List<byte[]>, List<byte[]>, List<byte[]>, List<byte[]>> response = contract.getAllClients().send();
+			configs = tuple7ToList(response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return configs;
+	}
+
+	private List<BootstrapConfigWeb3> tuple7ToList(Tuple7<List<byte[]>, List<byte[]>, List<byte[]>, List<byte[]>, List<byte[]>, List<byte[]>, List<byte[]>> tuple){
+		List<BootstrapConfigWeb3> bootstrapConfigs = new ArrayList<BootstrapConfigWeb3>();
+
+		List<byte[]> endpoints = tuple.component1();
+		List<byte[]> urls_bs = tuple.component2();
+		List<byte[]> ids_bs = tuple.component3();
+		List<byte[]> keys_bs = tuple.component4();
+		List<byte[]> urls_s = tuple.component5();
+		List<byte[]> ids_s = tuple.component6();
+		List<byte[]> keys_s = tuple.component7();
+
+		for (int i = 0; i < tuple.component1().size(); i++) {
+			String endpoint = byteToAscii(endpoints.get(i));
+			String url_bs = byteToAscii(urls_bs.get(i));
+			String id_bs = byteToAscii(ids_bs.get(i));
+			String key_bs = byteToAscii(keys_bs.get(i));
+			String url_s = byteToAscii(urls_s.get(i));
+			String id_s = byteToAscii(ids_s.get(i));
+			String key_s = byteToAscii(keys_s.get(i));
+
+			BootstrapConfigWeb3 bootstrapConfig = new BootstrapConfigWeb3(endpoint, url_bs, id_bs, key_bs, url_s, id_s, key_s);
+			bootstrapConfigs.add(bootstrapConfig);
+		}
+		return bootstrapConfigs;
+	}
+	
 	private byte[] asciiToByte32(String ascii){
 		byte[] bytearray = new byte[32];
 		for (int i = 0; i < ascii.length(); i++) {
