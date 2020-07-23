a0c766ecc4350c43635b9048389f296919911023
Ivan <ivaann2706@gmail.com>
2020-07-23 17:51:09 +0200

    EVALUATION - Clean up

--- evaluation/tests/src/main/java/utils/DeployContract.java
+++ evaluation/tests/src/main/java/utils/DeployContract.java
@@ -23,9 +23,9 @@ public class DeployContract {
 	public static void main(String[] args) {
 
 		System.out.println("BEGIN deployContract");
-		//Ropsten
-		String url = "https://ropsten.infura.io/v3/a21979a509154e19b42267c28f697e32";
-		String privateKey = "4C2A99F86C06C98448AB1986D33A248D699B5D7280EEBD76E4FD60B84C4B51C8"; 
+
+		String url = ""; //TO COMPLETE
+		String privateKey = ""; //TO COMPLETE
 
 		BigInteger gasPrice = new BigInteger("20000000000");
 		BigInteger gasLimit = new BigInteger("4712388");
@@ -42,7 +42,7 @@ public class DeployContract {
 //			clientContract = ClientStore.deploy(web3j, credentials, gasProvider, new ArrayList<String>()).send(); 
 //			userContract = UserStore.deploy(web3j, credentials, gasProvider, new ArrayList<String>()).send();
 //			anomalyContract = AnomalyStore.deploy(web3j, credentials, gasProvider, new ArrayList<String>()).send();
-//
+
 //			System.out.println("ClientContract Address: " + clientContract.getContractAddress());
 //			System.out.println("UserContract Address: " + userContract.getContractAddress());
 //			System.out.println("AnomalyContract Address: " + anomalyContract.getContractAddress());
@@ -50,6 +50,7 @@ public class DeployContract {
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		System.out.println("END deployContract");
 	}
 }
