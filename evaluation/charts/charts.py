42bed287b9110677c9ca2937dfb5f26cc7c64014
JTrillo <trillo@uma.es>
2020-07-27 16:56:30 +0200

    EVALUATION - Changes before article revision

--- evaluation/charts/charts.py
+++ evaluation/charts/charts.py
@@ -5,18 +5,21 @@ N_ANOMALIES = [10, 25, 50, 100]
 N_ANOMALIES_BAR = ["10", "25", "50", "100"]
 N_USERS = [10, 25, 50, 100]
 N_USERS_BAR = ["10", "25", "50", "100"]
-LINE_DASHED = dict(dash='dash')
+#PRIMARY_PLOTLY = "#8395c1" #muted blue
+ROYAL_BLUE = '#1f77b4' #royal blue
+BLUE_LINE = dict(color=ROYAL_BLUE)
+RED_LINE = dict(color='#d62728', dash='dash') #red brick
 
 # ClientStore charts
 
-def jsonAndBc(json, bc):
+def leshanVsProposed(json, bc):
   fig = go.Figure()
 
-  fig.add_trace(go.Scatter(x=N_CLIENTS, y=json, mode="lines", name="JSON"))
-  fig.add_trace(go.Scatter(x=N_CLIENTS, y=bc, mode="lines", name="BLOCKCHAIN", line=LINE_DASHED))
+  fig.add_trace(go.Scatter(x=N_CLIENTS, y=json, mode="lines", name="ECLIPSE LESHAN", line=BLUE_LINE))
+  fig.add_trace(go.Scatter(x=N_CLIENTS, y=bc, mode="lines", name="PROPOSED SOLUTION", line=RED_LINE))
 
-  fig.update_xaxes(title_text="Number of clients retrieved", range=[10, 500], tick0=10, dtick=49)
-  fig.update_yaxes(title_text="Total elapsed time (ms)", range=[0, 500])
+  fig.update_xaxes(title_text="<b>Number of clients retrieved</b>", range=[10, 500], tick0=10, dtick=49)
+  fig.update_yaxes(title_text="<b>Total elapsed time (ms)</b>", range=[0, 500])
 
   fig.layout.update(
     legend=dict(y=1.15, orientation="h"),
@@ -24,21 +27,21 @@ def jsonAndBc(json, bc):
   )
 
   config = {'toImageButtonOptions': {
-    'filename': f'jsonAndBc',
+    'filename': f'leshanVsProposed',
   }}
 
-  #fig.write_image("jsonAndBc.pdf")
+  #fig.write_image("leshanVsProposed.pdf")
 
   fig.show(config=config)
 
 def clientAddAndRemove(add, remove):
   fig = go.Figure()
 
-  fig.add_trace(go.Scatter(x=N_CLIENTS, y=add, mode="lines", name="ADD"))
-  fig.add_trace(go.Scatter(x=N_CLIENTS, y=remove, mode="lines", name="REMOVE", line=LINE_DASHED))
+  fig.add_trace(go.Scatter(x=N_CLIENTS, y=add, mode="lines", name="ADD", line=BLUE_LINE))
+  fig.add_trace(go.Scatter(x=N_CLIENTS, y=remove, mode="lines", name="REMOVE", line=RED_LINE))
 
-  fig.update_xaxes(title_text="Number of clients", range=[10, 500], tick0=10, dtick=49)
-  fig.update_yaxes(title_text="Average time per operation (s)", range=[0, 40], tick0=0, dtick=10)
+  fig.update_xaxes(title_text="<b>Number of clients</b>", range=[10, 500], tick0=10, dtick=49)
+  fig.update_yaxes(title_text="<b>Average time per operation (s)</b>", range=[0, 40], tick0=0, dtick=10)
 
   fig.layout.update(
     legend=dict(y=1.15, orientation="h"),
@@ -55,13 +58,13 @@ def clientAddAndRemove(add, remove):
 
 # AnomalyStore charts
 
-def addAnomalyLine(anomalies):
+def addCriticalInformationLine(anomalies):
   fig = go.Figure()
 
-  fig.add_trace(go.Scatter(x=N_ANOMALIES, y=anomalies, mode="lines"))
+  fig.add_trace(go.Scatter(x=N_ANOMALIES, y=anomalies, mode="lines", line=BLUE_LINE))
 
-  fig.update_xaxes(title_text="Anomalies added", range=[10, 100], tick0=10, dtick=10)
-  fig.update_yaxes(title_text="Average time per operation (s)", range=[0, 40], tick0=0, dtick=10)
+  fig.update_xaxes(title_text="<b>Critical information entries added</b>", range=[10, 100], tick0=10, dtick=10)
+  fig.update_yaxes(title_text="<b>Average time per operation (s)</b>", range=[0, 40], tick0=0, dtick=10)
 
   fig.layout.update(
     legend=dict(y=1.15, orientation="h"),
@@ -69,20 +72,20 @@ def addAnomalyLine(anomalies):
   )
 
   config = {'toImageButtonOptions': {
-    'filename': f'addAnomaly',
+    'filename': f'addCriticalInformation',
   }}
 
-  #fig.write_image("addAnomalyLine.pdf")
+  #fig.write_image("addCriticalInformationLine.pdf")
 
   fig.show(config=config)
 
-def addAnomaly(anomalies):
+def addCriticalInformation(anomalies):
   fig = go.Figure()
 
-  fig.add_trace(go.Bar(x=N_ANOMALIES_BAR, y=anomalies))
+  fig.add_trace(go.Bar(x=N_ANOMALIES_BAR, y=anomalies, marker_color=ROYAL_BLUE))
 
-  fig.update_xaxes(title_text="Anomalies added", type="category")
-  fig.update_yaxes(title_text="Average time per operation (s)", range=[0, 35], tick0=0, dtick=5)
+  fig.update_xaxes(title_text="<b>Critical information entries added</b>", type="category")
+  fig.update_yaxes(title_text="<b>Average time per operation (s)</b>", range=[0, 35], tick0=0, dtick=5)
 
   fig.layout.update(
     legend=dict(y=1.15, orientation="h"),
@@ -91,20 +94,20 @@ def addAnomaly(anomalies):
   )
 
   config = {'toImageButtonOptions': {
-    'filename': f'addAnomalyBar',
+    'filename': f'addCriticalInformationBar',
   }}
 
-  #fig.write_image("addAnomaly.pdf")
+  fig.write_image("addCriticalInformation.pdf")
 
   fig.show(config=config)
 
-def getAllAnomaliesLine(anomalies):
+def getAllCriticalInformationLine(anomalies):
   fig = go.Figure()
 
-  fig.add_trace(go.Scatter(x=N_ANOMALIES, y=anomalies, mode="lines"))
+  fig.add_trace(go.Scatter(x=N_ANOMALIES, y=anomalies, mode="lines", line=BLUE_LINE))
 
-  fig.update_xaxes(title_text="Anomalies retrieved", range=[10, 100], tick0=10, dtick=10)
-  fig.update_yaxes(title_text="Total time (ms)", range=[0, 500])
+  fig.update_xaxes(title_text="<b>Critical information entries retrived</b>", range=[10, 100], tick0=10, dtick=10)
+  fig.update_yaxes(title_text="<b>Total time (ms)</b>", range=[0, 500])
 
   fig.layout.update(
     legend=dict(y=1.15, orientation="h"),
@@ -112,20 +115,20 @@ def getAllAnomaliesLine(anomalies):
   )
 
   config = {'toImageButtonOptions': {
-    'filename': f'getAllAnomalies',
+    'filename': f'getAllCriticalInformation',
   }}
 
-  #fig.write_image("getAllAnomaliesLine.pdf")
+  #fig.write_image("getAllCriticalInformationLine.pdf")
 
   fig.show(config=config)
 
-def getAllAnomalies(anomalies):
+def getAllCriticalInformation(anomalies):
   fig = go.Figure()
 
-  fig.add_trace(go.Bar(x=N_ANOMALIES_BAR, y=anomalies))
+  fig.add_trace(go.Bar(x=N_ANOMALIES_BAR, y=anomalies, marker_color=ROYAL_BLUE))
 
-  fig.update_xaxes(title_text="Anomalies retrieved", type="category")
-  fig.update_yaxes(title_text="Total time (ms)", range=[0, 250], tick0=0, dtick=50)
+  fig.update_xaxes(title_text="<b>Critical information entries retrived</b>", type="category")
+  fig.update_yaxes(title_text="<b>Total time (ms)</b>", range=[0, 250], tick0=0, dtick=50)
 
   fig.layout.update(
     legend=dict(y=1.15, orientation="h"),
@@ -134,10 +137,10 @@ def getAllAnomalies(anomalies):
   )
 
   config = {'toImageButtonOptions': {
-    'filename': f'getAllAnomaliesBar',
+    'filename': f'getAllCriticalInformationBar',
   }}
 
-  #fig.write_image("getAllAnomalies.pdf")
+  fig.write_image("getAllCriticalInformation.pdf")
 
   fig.show(config=config)
 
@@ -146,11 +149,11 @@ def getAllAnomalies(anomalies):
 def usersAddAndUpdate(add, update):
   fig = go.Figure()
 
-  fig.add_trace(go.Scatter(x=N_USERS, y=add, mode="lines", name="ADD"))
-  fig.add_trace(go.Scatter(x=N_USERS, y=update, mode="lines", name="UPDATE", line=LINE_DASHED))
+  fig.add_trace(go.Scatter(x=N_USERS, y=add, mode="lines", name="ADD", line=BLUE_LINE))
+  fig.add_trace(go.Scatter(x=N_USERS, y=update, mode="lines", name="UPDATE", line=RED_LINE))
 
-  fig.update_xaxes(title_text="Number of clients", range=[10, 100], tick0=10, dtick=10)
-  fig.update_yaxes(title_text="Average time per operation (s)", range=[0, 40], tick0=0, dtick=10)
+  fig.update_xaxes(title_text="<b>Number of users</b>", range=[10, 100], tick0=10, dtick=10)
+  fig.update_yaxes(title_text="<b>Average time per operation (s)</b>", range=[0, 40], tick0=0, dtick=10)
 
   fig.layout.update(
     legend=dict(y=1.15, orientation="h"),
@@ -168,10 +171,10 @@ def usersAddAndUpdate(add, update):
 def validateLogin(validations):
   fig = go.Figure()
 
-  fig.add_trace(go.Bar(x=N_USERS_BAR, y=validations))
+  fig.add_trace(go.Bar(x=N_USERS_BAR, y=validations, marker_color=ROYAL_BLUE))
 
-  fig.update_xaxes(title_text="Users stored on smart contract", type="category")
-  fig.update_yaxes(title_text="Average time per login attempt (ms)", range=[0, 200], tick0=0, dtick=50)
+  fig.update_xaxes(title_text="<b>Users stored on smart contract</b>", type="category")
+  fig.update_yaxes(title_text="<b>Average time per login attempt (ms)</b>", range=[0, 200], tick0=0, dtick=50)
 
   fig.layout.update(
     legend=dict(y=1.15, orientation="h"),
